62439d1ca32f6b202b958c808883201e544a8688
Keith <keithwesley187@gmail.com>
2022-07-14 13:42:07 +0300

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,25 @@
 // Code EyesOnMe Component Here
+function EyesOnMe(){
+    function formSubmit(e){
+        e.preventDefault();
+    }
+    function focusEvent(){
+        console.log("Good!")
+    }
+    function blurEvent(){
+        console.log("Hey! Eyes on me!")
+    }
+    return(
+        <form onSubmit={formSubmit}>
+            <button 
+            onFocus={focusEvent}
+            onBlur={blurEvent}
+            >
+                Eyes on me
+
+            </button>
+        </form>
+    )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,20 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad(){
+    function formSubmit(e){
+        e.preventDefault();
+    }
+    function keyChange(){
+        console.log(`Entering password...`);
+    }
+    return (
+        <div>
+            <form onSubmit={formSubmit}>
+                <input name="" type="password" onChange={keyChange}/>
+            </form>
+        </div>
+    )
+}
+
+export default Keypad;
